6231ff0119a873bc79d8551c896e758f2231db8a
HarshaSalih <harshasalih2000@gmail.com>
2022-12-06 11:40:43 +0530

    addcourse commits

--- src/main/java/com/example/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/example/courseapp_backend/controller/CourseController.java
@@ -1,17 +1,24 @@
 package com.example.courseapp_backend.controller;
 
+import com.example.courseapp_backend.model.Courses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class CourseController {
 
-    @PostMapping("/")
-    public String Addcourse()
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String Addcourse(@RequestBody Courses c)
     {
-        return "course added";
+        System.out.println(c.getCourseTitle().toString());
+        System.out.println(c.getCourseDescription().toString());
+        System.out.println(c.getCourseVenue().toString());
+        System.out.println(c.getCourseDuration().toString());
+        System.out.println(c.getCourseDate().toString());
+        return "course added successfully";
     }
 
     @GetMapping("/view")
