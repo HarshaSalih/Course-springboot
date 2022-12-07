dfc4bb89e9db4cc8a305564bafa019c35910d7db
HarshaSalih <harshasalih2000@gmail.com>
2022-12-07 17:34:19 +0530

    commits

--- src/main/java/com/example/courseapp_backend/dao/CourseDao.java
+++ src/main/java/com/example/courseapp_backend/dao/CourseDao.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface CourseDao extends CrudRepository<Courses,Integer> {
 
-    @Query(value = "SELECT `id`, `course_date`, `course_description`, `course_duration`, `course_title`, `course_venue` FROM `courses` WHERE `course_title` = :course_title",nativeQuery = true)
+    @Query(value = "SELECT `id`, `course_date`, `course_description`, `course_duration`, `course_title`, `course_venue` FROM `courses` WHERE `course_title` LIKE %:course_title%",nativeQuery = true)
     List<Courses> SearchCourse(@Param("course_title")String course_title);
 }
